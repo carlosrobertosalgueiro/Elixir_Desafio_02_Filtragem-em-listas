0d65763bf6425be5397e485f4b35a797cc76053f
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-19 09:35:11 -0300

    valid inserted test

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,13 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "when a valid list is provided, returns the number of odd numbers" do
+      response = ["1", "2", "carlos", "3", "4", "5", "text"]
+
+      expected_response = ["1", "2", "carlos", "3", "4", "5", "text"]
+
+      assert response == expected_response
+    end
   end
 end
